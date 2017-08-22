c729130494c8913a826e95c7219d87d28fcb1ba0
Amed <amed157@hotmail.com>
2017-08-22 11:47:22 -0500

    change structure

--- test/controllers/warehouses_controller_test.rb
+++ test/controllers/warehouses_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class DemaWarehouseTest < ActiveSupport::TestCase
+class WarehousesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
